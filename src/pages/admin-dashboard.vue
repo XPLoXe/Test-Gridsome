fa7360193d65bdb704c97c3b22501ee2badb1563
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-10-10 10:35:52 +0200

    fix

---
+++ src/pages/admin-dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <AdminPage></AdminPage>
+
+    <!-- Default content for /admin when there's no nested route -->
+    This is the admin page.
+    <br />
+    <g-link to="/admin/textseiten">Go to Textseiten</g-link>
+    <br />
+    <g-link :to="'/admin/textseiten/' + page.id">Go to Textseiten by id</g-link>
+  </Layout>
+</template>
+
+<script>
+import AdminPage from "../components/AdminPage.vue";
+
+//initialize vue2 data
+export default {
+  name: "admin-dashboard",
+  components: {
+    AdminPage,
+  },
+  data() {
+    return {
+      page: {
+        id: "textseiten",
+      },
+    };
+  },
+};
+</script>
