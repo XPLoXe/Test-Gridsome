160d7056316f8d8eddfb4f75e32ae9d770384028
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-10-10 10:39:42 +0200

    chat GPT fix

--- src/pages/admin-dashboard.vue
+++ src/pages/admin-dashboard.vue
@@ -1,13 +1,7 @@
 <template>
   <Layout>
-    <AdminPage></AdminPage>
-
-    <!-- Default content for /admin when there's no nested route -->
     This is the admin page.
-    <br />
-    <g-link to="/admin/textseiten">Go to Textseiten</g-link>
-    <br />
-    <g-link :to="'/admin/textseiten/' + page.id">Go to Textseiten by id</g-link>
+    <g-link to="/admin/text/">Go to Text</g-link>
   </Layout>
 </template>
 
