844e1eb2e35b7a9cc1345a2fec9f185314c5abd5
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-10-10 10:54:18 +0200

    ChatGPT fix 4

--- src/pages/admin-dashboard.vue
+++ src/pages/admin-dashboard.vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
     <slot></slot>
+    <g-link to="/admin">Go to Admin Index</g-link>
+    <br />
     <g-link to="/admin/text">Go to Text</g-link>
     <br />
     <g-link to="/approach">Go to Approach</g-link>
---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    This is the Admin Index Page.
+  </div>
+</template>
